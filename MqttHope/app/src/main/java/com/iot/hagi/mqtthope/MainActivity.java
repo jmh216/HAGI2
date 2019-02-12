a476df48cd03cc2081de431534462a48622bef3b
Jimmy Hu <747785928hjm@gmail.com>
2019-02-12 23:11:31 +0000

    line chart of distance

--- MqttHope/app/src/main/java/com/iot/hagi/mqtthope/MainActivity.java
+++ MqttHope/app/src/main/java/com/iot/hagi/mqtthope/MainActivity.java
@@ -5,14 +5,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import helpers.MqttHelper;
+import helpers.ChartHelper;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.lang.Math.*;
 
 public class MainActivity extends AppCompatActivity {
     MqttHelper mqttHelper;
+    ChartHelper mChart;
+    LineChart chart;
 
     TextView dataReceived;
 
@@ -23,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         dataReceived = (TextView) findViewById(R.id.dataReceived);
+        chart = (LineChart) findViewById(R.id.chart);
+        mChart = new ChartHelper(chart);
 
         startMqtt();
     }
@@ -42,11 +53,44 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
-                Log.w("Debug", mqttMessage.toString());
-                dataReceived.setText(mqttMessage.toString());
+
+//                String JSON_STRING  =  mqttMessage.toString();
+//
+//                Log.w("received", JSON_STRING);
+//
+//                dataReceived.setText(JSON_STRING);
+//
+//                mChart.addEntry(Float.valueOf(JSON_STRING));
+
+                String jsonString = mqttMessage.toString();
+
+                JSONObject objson = new JSONObject(jsonString);
+                String dist = objson.getString("distance");
+
+                Log.w("JSON", jsonString);
+//                Log.w("JSON", dist);
+
+                float fdist = Math.min(Float.valueOf(dist), 1300.0f);
+
+                if( fdist == 1300f)
+                {
+//                    String fdist = " out of range ";
+                    dataReceived.setText("Distance out of range.");
+                }
+                else
+                {
+                    dataReceived.setText("distance : " + fdist);
+
+                }
+                Log.w("JSON", Float.toString(fdist));
+                mChart.addEntry(fdist);
+
+
+
             }
 
             @Override
+
             public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
 
             }
